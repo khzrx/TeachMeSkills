b614bef28e5a7f7a9d1465b730cd37bbe1c588e6
Goncharenko Filipp <phil.can@yandex.ru>
2023-04-24 23:30:39 +0300

    Homework 8

---
+++ Home8/Person.cs
@@ -0,0 +1,42 @@
+﻿namespace Home8
+{
+    public class Person
+    {
+        public string PersonName { get; set; }
+        public int CaloriesLimit { get; set; }
+        public List<Ration> RationForWeek { get; set; }
+
+        public Person(string personName, int caloriesLimit)
+        {
+            PersonName = personName;
+            CaloriesLimit = caloriesLimit;
+        }
+
+        public Person(string personName, int caloriesLimit, List<Ration> rationForWeek)
+        {
+            PersonName = personName;
+            CaloriesLimit = caloriesLimit;
+            RationForWeek = rationForWeek;
+        }
+
+        public List<Ration> CheckRation(Person person)
+        {
+            int rationCount = person.RationForWeek.Count();
+            for (int i = 0; i < rationCount - 1; i++)
+            {
+                RationCreator rationCreator = new RationCreator();
+                int caloriesSumInRation = rationCreator.CaloriesSumInRation(RationForWeek[i]);
+
+                while (caloriesSumInRation > person.CaloriesLimit)
+                {
+                    List<Ration> rationList = person.RationForWeek;
+                    List<Product> productList = rationList[i].ProductList;
+                    productList.RemoveAt(0);
+                    caloriesSumInRation = rationCreator.CaloriesSumInRation(RationForWeek[i]);
+                }
+            }
+
+            return person.RationForWeek;
+        }
+    }
+}
---
+++ Home8/Ration.cs
@@ -0,0 +1,14 @@
+﻿namespace Home8
+{
+    public class Ration
+    {
+        public DayOfTheWeek Day { get; set; }
+        public List<Product> ProductList { get; set; }
+
+        public Ration(DayOfTheWeek day, List<Product> productList)
+        {
+            Day = day;
+            ProductList = productList;
+        }
+    }
+}
