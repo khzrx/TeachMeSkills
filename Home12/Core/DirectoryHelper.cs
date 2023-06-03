ba36758106ce9d9d187e7713c93b4a29b93df58a
Goncharenko Filipp <phil.can@yandex.ru>
2023-06-03 11:12:44 +0300

    Upload test added.

---
+++ Home12/Core/DirectoryHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home12.Core
+{
+    internal class DirectoryHelper
+    {
+        public static string GetTestDataFolderPath()
+        {
+            return Environment.CurrentDirectory + "\\TestData\\";
+        }
+    }
+}
